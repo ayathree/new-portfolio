48fc83431ef4694e04172dca95e26b1132d6b3d6
ayathree <nobanitaayathree333@gmail.com>
2024-11-28 14:16:27 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,40 +3,45 @@
 import { ReactTyped } from 'react-typed'
 import './App.css'
 import portfolioImage from './assets/file.png'
+import Lottie from 'lottie-react'
+import animation from './assets/Animation - 1732779013753.json'
 
 function App() {
   
 
   return (
     <div className='bg-neutral-900 min-h-screen grid grid-cols-1 grid-rows-5 lg:grid-cols-3 lg:grid-rows-2 p-6 gap-6'>
-      <div className='  border-2 border-orange-600   lg:row-span-2 h-full flex justify-center items-center block lg:hidden '>
-        <img className='bg-black p-2 h-full w-full object-cover ' src={portfolioImage} alt="" />
-      </div>
-      <div className='flex justify-center items-center uppercase bg-neutral-800 text-center text-white font-bold text-3xl border-2 border-orange-600 '>about <span className='text-orange-600'>me</span></div>
-      <div className='uppercase  text-white font-bold text-3xl text-center flex justify-center items-center  '>Nobanita <span className='text-orange-600'> ayathree</span></div>
-      <div className='border-2 border-orange-600 text-center lg:row-span-2 h-full flex justify-center items-center hidden lg:block relative'>
-  <img className='bg-black p-2 h-full w-full object-cover' src={portfolioImage} alt="" />
+     <div className='  text-center  lg:row-span-2 h-full flex justify-center items-center  relative'>
+  <img className='bg-black  h-full w-full object-cover' src={portfolioImage} alt="" />
   <div className="absolute inset-0 bg-gradient-to-t from-black/40 via-black/70 to-transparent"></div>
-  <div className='absolute inset-0 flex flex-col justify-center items-center'>
-    <ReactTyped
-      className='uppercase font-bold text-xs md:text-2xl lg:text-3xl text-white text-center'
-      strings={[
-        "Search for products",
-        "Search for categories",
-        "Search for brands",
-      ]}
-      typeSpeed={40}
-      backSpeed={50}
-      attr="placeholder"
-      loop
-    >
-      <input type="text" className="bg-transparent text-white text-center  outline-none placeholder:text-white" />
-    </ReactTyped>
+  <div className='absolute  inset-0 flex justify-center items-center'>
+  <Lottie className=' w-1/2 ' animationData={animation}></Lottie>
   </div>
 </div>
 
-      <div className='flex justify-center items-center uppercase bg-neutral-800 text-center text-white font-bold text-3xl border-2 border-orange-600 '>my <span className='text-orange-600'>portfolio</span></div>
-      <div className='flex justify-center items-center uppercase bg-neutral-800 text-center text-white font-bold text-3xl border-2 border-orange-600'>get <span className='text-orange-600'>in touch</span></div>
+<div className="flex flex-col justify-center items-center">
+  {/* Display "Nobanita Ayathree" */}
+  <p className="text-white text-center text-xs lg:text-2xl  uppercase mb-2" >Hi there ! i'm</p>
+  <p 
+    className="text-white text-center text-2xl lg:text-4xl font-bold uppercase mb-2"
+  >nobanita ayathree
+    </p>
+ 
+
+  {/* Display "Web Developer" */}
+  <ReactTyped
+    strings={["Web Developer"]}
+    typeSpeed={40}
+    backSpeed={50}
+    loop
+    className="uppercase text-orange-400 text-center text-xs  lg:text-2xl font-bold"
+  />
+</div>
+
+      <div className='flex justify-center items-center uppercase bg-neutral-800 text-center text-white font-bold text-3xl    '>about <span className='text-orange-400 '>me</span></div>
+      
+      <div className='flex justify-center items-center uppercase bg-neutral-800 text-center text-white font-bold text-3xl   '>my <span className='text-orange-400'>portfolio</span></div>
+      <div className='flex justify-center items-center uppercase bg-neutral-800 text-center text-white font-bold text-3xl  '>get <span className='text-orange-400'>in touch</span></div>
       
 
     </div>
